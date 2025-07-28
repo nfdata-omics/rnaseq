8685c885600bcc16575d9b5e5a86270988f782d6
Giulia Scotti <giulia.scotti@fht.org>
2025-07-28 14:57:19 +0200

    test tryCatch on ora_clusterProfiler.R

--- bin/ora_clusterProfiler.R
+++ bin/ora_clusterProfiler.R
@@ -77,15 +77,25 @@ if(length(my_genes)>=5){
     print("The list of differentially expressed genes was longer than 10.000 genes, therefore it was truncated at 10.000 genes. 
             Consider to apply more stringent cutoffs on FDR and/or log2FC")
   }
-  my_enrich = enricher(gene=my_genes, universe=background_genes, maxGSSize=1000, TERM2GENE=my_gmt)
+  #my_enrich = enricher(gene=my_genes, universe=background_genes, maxGSSize=1000, TERM2GENE=my_gmt)
   # BgRatio = M/N, with M = #genes in the selected pathway that are also in the background; with N = #unique genes in all the gmt (all pathways) that are also in the background
   # GeneRatio = M/N, with M = #my input genes that are also in the selected pathway; with N = #my input genes that are also in all the gmt (all pathways)
-  if(!is.null(my_enrich)){
+  my_enrich = tryCatch({ 
+    enricher(gene=my_genes, universe=background_genes, maxGSSize=1000, TERM2GENE=my_gmt)}, 
+    error = function(e) {message("Enrichment failed: ", e$message)NULL
+  })
+  if (!is.null(my_enrich) && inherits(my_enrich, "enrichResult")) {
     write.table(x=my_enrich@result, file=paste(outname, ".", collection, ".all.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
   } else {
     no_enrichment = data.frame(empty="No enrichment was found.")
     write.table(x=no_enrichment, file=paste(outname, ".", collection, ".all.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
   }
+  #if(!is.null(my_enrich)){
+  #  write.table(x=my_enrich@result, file=paste(outname, ".", collection, ".all.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+  #} else {
+  #  no_enrichment = data.frame(empty="No enrichment was found.")
+  #  write.table(x=no_enrichment, file=paste(outname, ".", collection, ".all.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+  #}
 } else {
   no_enrichment = data.frame(empty=paste("Enrichment analysis was not performed. Less than 5 significant genes were retrieved using the cutoff FDR<", fdr, ", abs_log2FC>", logfc, ".", sep=""))
   write.table(x=no_enrichment, file=paste(outname, ".", collection, ".all.txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
