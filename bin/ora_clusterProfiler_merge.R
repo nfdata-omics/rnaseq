8bb554706985eb442bd13e58945fa6d993d03972
Matteo Bonfanti <matteo.bonfanti@fht.org>
2026-09-07 11:49:09 +0200

    Add length restriction for set_name variable
    Limit the length of set_name to 31 characters.

--- bin/ora_clusterProfiler_merge.R
+++ bin/ora_clusterProfiler_merge.R
@@ -63,6 +63,9 @@ for(f in files){
 		outname = paste0(paste(strsplit(gsub(suffix, "", basename(f)), "\\.")[[1]][1:2], collapse="."), ".")
 		set_name = gsub(suffix, "", gsub(outname, "", basename(f)))
 	}
+	if (nchar(set_name) > 31) {
+		set_name = substr(set_name, 1, 31)
+	}
 
   # Importing result table
   dat = read.delim(f, h=T)
